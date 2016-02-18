9549e8b6bc04485d3f6e8708c897e4e7b25b879c
vikashiscool <vikash_parekh@yahoo.com>
2016-02-17 21:53:57 -0800

    Fix navbar, portfolio modals, include jBox

--- app.js
+++ app.js
@@ -20,6 +20,35 @@ $(document).ready(function(){
   // I am a mentor.
   // I am Vikash.
 
+// new jBox('Modal', {
+//     width: 300,
+//     height: 200,
+//     attach: $('#resolv'),
+//     title: 'Resolv',
+//     content: '<i>Hello there!</i>'
+// });
+ 
 
 });
 
+
+
+//   <div class="modal fade" id="resolvModal" tabindex="-1" role="dialog">
+//   <div class="modal-dialog">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+//         <h4 class="modal-title">Resolv</h4>
+//       </div>
+//       <div class="modal-body">
+//         <p><b>Description: </b>A MEAN stack web app that allows users to track and share their New Years Resolutions.</p>
+//         <p><b>Technologies: </b>Angular/Bootstrap (front-end), Node/Express/MongoDB (back-end).</p>
+//         <p><b>Responsibilities: </b>All stages of development, from schema to deployment.</p>
+//         <p><b>Source: </b><a href="https://github.com/project-funtime/resolv" target="_blank">https://github.com/project-funtime/resolv</a></p>
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+//       </div>
+//     </div><!-- /.modal-content -->
+//   </div><!-- /.modal-dialog -->
+// </div><!-- /.modal -->
